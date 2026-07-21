0d3e4478ed06b3d918257f9f98a60f885d2f6b66
thebacco <thebacco@gmail.com>
2026-07-20 22:53:04 -0400

    Restructure fertilizer tab as action timeline

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "garden-notes-v7";
+const CACHE_NAME = "garden-notes-v8";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=7",
-  "./app.js?v=7",
+  "./styles.css?v=8",
+  "./app.js?v=8",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
