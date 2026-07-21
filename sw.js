b1db4b86c372e65cedb08aa1d379086c6ff861c7
thebacco <thebacco@gmail.com>
2026-07-20 22:54:51 -0400

    Replace feed chart with garden schedule tables

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "garden-notes-v8";
+const CACHE_NAME = "garden-notes-v9";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=8",
-  "./app.js?v=8",
+  "./styles.css?v=9",
+  "./app.js?v=9",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
