481a0db87fed937bc0e11138ae83b63544e36df9
thebacco <thebacco@gmail.com>
2026-07-20 23:01:47 -0400

    Use technical crop group labels

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "garden-notes-v10";
+const CACHE_NAME = "garden-notes-v11";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=10",
-  "./app.js?v=10",
+  "./styles.css?v=11",
+  "./app.js?v=11",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
