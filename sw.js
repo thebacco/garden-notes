2f6cd9ec662006b344c302e74663ec8839ec76cc
thebacco <thebacco@gmail.com>
2026-07-20 23:06:05 -0400

    Add chart definitions and technical corrections

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "garden-notes-v11";
+const CACHE_NAME = "garden-notes-v12";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=11",
-  "./app.js?v=11",
+  "./styles.css?v=12",
+  "./app.js?v=12",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
