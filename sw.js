abbecff20b078cadf0745097b5e84ed29462aed9
thebacco <thebacco@gmail.com>
2026-07-20 23:26:26 -0400

    Update strawberry risk guidance in feed chart

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "garden-notes-v12";
+const CACHE_NAME = "garden-notes-v13";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=12",
-  "./app.js?v=12",
+  "./styles.css?v=13",
+  "./app.js?v=13",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
