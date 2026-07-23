9f196c3f119b337eea787c6a9e099c1a26fb8bde
thebacco <thebacco@gmail.com>
2026-07-22 23:07:09 -0400

    Distinguish My Crops tab

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "garden-notes-v17";
+const CACHE_NAME = "garden-notes-v18";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=17",
-  "./app.js?v=17",
+  "./styles.css?v=18",
+  "./app.js?v=18",
   "./manifest.webmanifest",
   "./icon-192.png",
   "./icon-512.png"
